e671126c14690c384e7a48a8021ea683427e361b
dayan2222 <dayanahmed2222@gmail.com>
2020-07-09 01:00:01 +0500

    List View

---
+++ list_view/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  List<String> articleNames = ['Mouse','Keyboard','Hard Disk','RAM','Processor','Monitor','Speaker','Mouse','Keyboard','Hard Disk','RAM','Processor','Monitor','Speaker'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ListView",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List View"),
+        ),
+        body: ListView.separated(
+          itemCount: articleNames.length,
+
+
+          itemBuilder: (BuildContext context, int index){
+            return ListTile(
+              title: Text(articleNames[index], style: TextStyle(fontSize: 16.0, color: Colors.grey[800]),),
+            );
+          },
+
+          separatorBuilder: (BuildContext context, int index)=>Divider(),
+        ),
+      ),
+    );
+  }
+}
